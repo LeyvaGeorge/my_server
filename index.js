1c54a48c466f1e76975cc5e8dab2b3450904057e
George Leyva <leyvageorge@hotmail.com>
2023-03-19 15:51:32 -0700

    Adding default view for layout

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ require('dotenv').config()
 const express = require('express')
 const server = express()
 
+server.use('/places', require('./controllers/places'))
+
 server.get('/',(req,res) => {
     res.send ('HelloWorld!')
 })
+
 server.get('*', (req,res) => {
     res.status(404).send('<h1>404 Page</h1>')
 })
+
 server.listen(process.env.PORT,function () {
     console.log("Server running: 8080")
 })
