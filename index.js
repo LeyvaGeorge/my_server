03b244914bfe18362a681895220cf8b71b0cbc0c
George Leyva <leyvageorge@hotmail.com>
2023-03-20 15:28:02 -0700

    adding styles to myServer

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+//Modules and Globals
 require('dotenv').config()
 const express = require('express')
 const server = express()
-//Dif of the view engine jsx
+
+//Express Setting
+server.set('views',__dirname + '/views')
 server.set('view engine','jsx')
 server.engine('jsx',require('express-react-views').createEngine())
+server.use(express.static('public'))
 
+//=====  Controllers & Routes  =====
 server.use('/places', require('./controllers/places'))
 
 //sends to home
@@ -17,6 +22,7 @@ server.get('*', (req,res) => {
     res.render('error404')
 })
 
+//Listen for Connections
 server.listen(process.env.PORT,function () {
     console.log("Server running: 8080")
 })
--- views/default.jsx
+++ views/default.jsx
@@ -1,9 +1,14 @@
+//This page creates the template for all other pages
 const React = require('react')
 function Def (html) {
     return (
         <html>
         <head>
             <title>Title</title>
+            {/* Bootstrap */}
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" />
+            {/* Style */}
+            <link rel="stylesheet" href="/css/style.css" />
         </head>
         <body>
             {html.children}
