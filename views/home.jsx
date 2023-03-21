1197c69b32e739d7b27f5b096c9a7a8cc13e1d27
George Leyva <leyvageorge@hotmail.com>
2023-03-20 21:26:11 -0700

    Finished pt.5

--- views/home.jsx
+++ views/home.jsx
@@ -7,15 +7,21 @@ function home () {
       <Def>
           <main>
               <h1>HOME</h1>
+
+              <a href="/places/new">
+                <button className='btn-primary'>New</button>
+              </a>
+              <a href="/places">
+                <button className="btn-primary">Places</button>
+              </a>
+              
               <div>
                 <img src="https://place-puppy.com/300x300" alt="Puppy" />
                 <div>
                   <p>This a place holder for reference</p>
                 </div>
               </div>
-              <a href="/places">
-                <button className="btn-primary">Places</button>
-              </a>
+              
           </main>
       </Def>
     )
