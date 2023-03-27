9134874e0731e4be330778dc9f28a1d27e703cbe
George Leyva <leyvageorge@hotmail.com>
2023-03-26 19:18:04 -0700

    pt9.section3 Done

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,26 @@ const React = require('react')
 const Def = require('../default')
 
 function show (data) {
+    let comments = (
+        <h3 className='inactive'>
+            No comments yet!
+        </h3>
+        
+    )
+    if (data.place.comments.length) {
+            comments = data.place.comments.map(c => {
+                return (
+                    <div className='border'>
+                        <h2 className='rant'>{c.rant ? 'Rant!' : 'Rave!'}</h2>
+                        <h4>{c.author}</h4>
+                        <h3>
+                            <strong> {c.author}</strong>
+                        </h3>
+                        <h4>Rating: {c.stars}</h4>
+                    </div>
+                )
+            })
+        }
     return (
         <Def>
           <main>
@@ -41,7 +61,7 @@ function show (data) {
                     </form>
                 </div>
                 <h2>Comments</h2>
-                <p>No comments yet!</p> 
+                {comments}
             </div>
           </main>
         </Def>
